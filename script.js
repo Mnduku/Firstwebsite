b085d32915dfffdd58026dc8065e998841150dfc
Mike <116856099+Mnduku@users.noreply.github.com>
2023-11-06 14:34:44 -0500

    Delete script.js
    Signed-off-by: Mike <116856099+Mnduku@users.noreply.github.com>

--- script.js
+++
@@ -1,7 +0,0 @@
-window.onload=function(){
-    const btn = document.querySelector('#button')
-    btn.addEventListener('click', function (e) {
-        console.log(e.target);
-    });
-    console.log("hoal")
-}
