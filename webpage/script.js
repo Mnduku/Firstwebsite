0b8a43b27617404d2dd116b66aef0a464117f78b
mike <phlykes@gmail.com>
2023-11-06 14:26:13 -0500

    Organization changes

---
+++ webpage/script.js
@@ -0,0 +1,8 @@
+window.onload=function(){
+    const btn = document.querySelector('#button')
+    btn.addEventListener('click', function (e) {
+        console.log(e.target);
+    });
+
+    console.log("hoal")
+}
